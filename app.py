f0d2b7c99ab7138f8fa0e136248c366913fd7f72
nareshbandaru0505 <55139683+nareshbandaru0505@users.noreply.github.com>
2019-11-04 11:05:16 +0200

    adding updated app.py to repository

--- app.py
+++ app.py
@@ -3,19 +3,22 @@ from flask import Flask, request
 # abort, Resource, Api classes imported from flask_restful module inorder to create the first rest api
 from flask_restful import Resource, Api, abort
 
+#import JWT,jwt_required, current_identity from flask_jwt module which is used for authentication
 from flask_jwt import JWT, jwt_required, current_identity
+
+#import authenticate and identity from securty.py previously created.
 from security import authenticate, identity
 
 # Flask application instance is created using below command
 app = Flask(__name__)
 
 # Adding a secreate line
-#app.config['SECRET_KEY'] = 'FirstJson1'
 app.secret_key = 'FirstJson1'
 
 #Api application instance is created using below command
 api = Api(app)
 
+#create a JWT object. As soon as this JWT object created, Flask-JWT registers an endpoint with our application, /auth
 jwt = JWT(app, authenticate, identity)
 
 #defining total items
@@ -24,6 +27,8 @@ items = {}
 # generally API works with resources and resoruces should be defined as class. item and itemList is created as Resource class
 # Resource item definition
 # for GET and DELETE method, verifying if the  value exists. if doesnt exists, report error code 404 and print itemX doesnt exist message line 14
+# Inside any endpoint that is decorated with @jwt_required(), can access the current_identity proxy—
+# it will give us whatever the identity function returns for the JWT we received in this specific request.
 class item(Resource):
     # GET method to print an specific item
     # if the requted item doesnt exist, it will abort with an error with code 404
@@ -35,6 +40,7 @@ class item(Resource):
             abort(404, message="{} doesn't exist".format(name))
  
     #put method to create a new item or modify the existing
+    #call the identity function in our endpoints using @jwt_required() decorator
     @jwt_required()
     def put(self, name):
         items[name] = {'price' : request.form['price'] }
@@ -42,6 +48,7 @@ class item(Resource):
 
     # POST method to create a new endpoint with concrete price value as requested.
     # if the requted name already exist, it will abort and  print an error with error code 403
+    # call the identity function in our endpoints using @jwt_required() decorator
     @jwt_required()
     def post(self, name):
         if name not in items:
@@ -64,6 +71,7 @@ class item(Resource):
 # get method to print all the items
 
 class itemList(Resource):
+    #call the identity function in our endpoints using @jwt_required() decorator
     @jwt_required()
     def get(self):
         return items
